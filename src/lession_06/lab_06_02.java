50e3201f19514a036cd46a6908bde77c24a9bd09
oanhha218@gmail.com <oanhha218@gmail.com>
2022-09-29 22:51:45 +0700

    lession 6

--- src/lession_06/lab_06_02.java
+++ src/lession_06/lab_06_02.java
@@ -4,18 +4,20 @@ import java.util.Scanner;
 public class lab_06_02 {
     public static void main(String[] args) {
         String str = "Kimoanh218@@";
-        for (int passAttempts = 0; passAttempts < 3; passAttempts++)
-        {
+        final int try_enter = 2;
+        for (int passAttempts = 0; passAttempts <= 2; passAttempts++) {
             System.out.print("Nhap passWord: ");
             Scanner input = new Scanner(System.in);
             String inputPass = input.nextLine();
 
-            if(!(inputPass.equals(str))) {
+            if (!(inputPass.equals(str))) {
                 System.out.println("Password sai, vui long nhap lai");
-            } else
-            {
+            } else {
                 System.out.println("Wellcome");
                 break;
+            }
+            if (passAttempts == try_enter) {
+                System.out.println("You are reach maximum password tries");
             }
         }
     }
